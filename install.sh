e43019fb888c9a29c8a2e3e547b9bebfe387197b
Eli Jonas <ecjonas12@duck.com>
2026-02-10 11:41:53 -0500

    Fix typo in purge command for lightdm

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ sudo apt install -y task-xfce-desktop
 echo "Purging meta-package to prevent autostart issues..."
 sudo apt purge -y task-xfce-desktop
 sudo apt purge -y libreoffice*
+sudo apr purge lightdm
 
 # Create applications directory if it doesn't exist
 mkdir -p ~/.local/share/applications
